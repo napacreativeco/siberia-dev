ab14d8ca0c2022e7160910e2ffed8b36781e8efc
Throne <contact@throne.work>
2023-06-09 10:14:33 -0700

    adding netlify configs

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import hydrogen from '@shopify/hydrogen/plugin'
+import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
+
+export default defineConfig({
+  plugins: [hydrogen(), netlifyPlugin()],
+  //   ...
+})
